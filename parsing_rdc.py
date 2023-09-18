4896a446a9b07eaa0feb29550dbb5ca52c0c974e
spech13 <es-popov2@yandex.ru>
2023-09-18 08:52:41 +0300

    Black formating

--- parsing_rdc.py
+++ parsing_rdc.py
@@ -1,15 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-class New:
-	def __init__(self, href, title):
-		self.href = href
-		self.title = title
 
-	def get_content(self):
-		page = requests.get(self.href)
-		soup = BeautifulSoup(page.text, "html.parser")
-		return "\n".join(item.text.replace("\n", "") for item in soup.findAll('p')) 
+class New:
+    def __init__(self, href, title):
+        self.href = href
+        self.title = title
+
+    def get_content(self):
+        page = requests.get(self.href)
+        soup = BeautifulSoup(page.text, "html.parser")
+        return "\n".join(item.text.replace("\n", "") for item in soup.findAll("p"))
 
 
 url = "https://rbc.ru"
@@ -17,12 +18,11 @@ url = "https://rbc.ru"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 news = [
-		New(href.attrs["href"], href.findAll(class_="main__feed__title")[0].text)
-		for href in soup.findAll
-			(
-				'a', class_="main__feed__link js-yandex-counter js-visited"
-			)
-	]
+    New(href.attrs["href"], href.findAll(class_="main__feed__title")[0].text)
+    for href in soup.findAll(
+        "a", class_="main__feed__link js-yandex-counter js-visited"
+    )
+]
 
 for num, new in enumerate(news):
-	print(f"{num}.{new.title}\n\n{new.get_content()}\n")
+    print(f"{num}.{new.title}\n\n{new.get_content()}\n")
